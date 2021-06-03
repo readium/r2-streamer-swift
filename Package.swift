04d63cca3f38a4d9d86a90474235b896c3ef9a0c
Steven Zeck <8315038+stevenzeck@users.noreply.github.com>
2021-06-02 21:02:19 -0500

    Add GCDWebServer, temporarily use branch for R2Shared

--- Package.swift
+++ Package.swift
@@ -17,14 +17,15 @@ let package = Package(
         .package(url: "https://github.com/marmelroy/Zip.git", .exact("2.1.1")),
         .package(url: "https://github.com/cezheng/Fuzi.git", .exact("3.1.3")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.3.8")),
-        .package(name: "R2Shared", path: "../r2-shared-swift")
+        .package(name: "R2Shared", url: "https://github.com/stevenzeck/r2-shared-swift.git", .branch("use-spm")),
+        .package(name: "GCDWebServer", url: "https://github.com/stevenzeck/GCDWebServer.git", .branch("use-spm"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "R2Streamer",
-            dependencies: ["Zip", "Fuzi", "CryptoSwift", "R2Shared"],
+            dependencies: ["Zip", "Fuzi", "CryptoSwift", "R2Shared", "GCDWebServer"],
             path: "./r2-streamer-swift/",
             exclude: ["Info.plist"],
             resources: [
