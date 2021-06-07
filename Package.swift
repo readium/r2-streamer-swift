618f2b285b84a9fbd5f1d52423954aa5642e119a
Mickaël Menu <mickael.menu@gmail.com>
2021-06-07 17:36:44 +0200

    Update Package.swift and changelog

--- Package.swift
+++ Package.swift
@@ -8,25 +8,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "R2Streamer",
+    name: "r2-streamer-swift",
     defaultLocalization: "en",
     platforms: [.iOS(.v10)],
     products: [
-        .library(
-            name: "R2Streamer",
-            targets: ["R2Streamer"]),
+        .library(name: "R2Streamer", targets: ["R2Streamer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/marmelroy/Zip.git", .exact("2.1.1")),
-        .package(url: "https://github.com/cezheng/Fuzi.git", .exact("3.1.3")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .exact("1.3.8")),
-        .package(name: "R2Shared", url: "https://github.com/readium/r2-shared-swift.git", .branch("develop")),
-        .package(name: "GCDWebServer", url: "https://github.com/readium/GCDWebServer.git", from: "3.7.0"),
+        .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.1"),
+        .package(url: "https://github.com/cezheng/Fuzi.git", from: "3.1.3"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.8"),
+        .package(url: "https://github.com/readium/r2-shared-swift.git", .branch("develop")),
+        .package(url: "https://github.com/readium/GCDWebServer.git", from: "3.7.0"),
     ],
     targets: [
         .target(
             name: "R2Streamer",
-            dependencies: ["Zip", "Fuzi", "CryptoSwift", "R2Shared", "GCDWebServer"],
+            dependencies: [
+                "CryptoSwift",
+                "Fuzi",
+                "GCDWebServer",
+                .product(name: "R2Shared", package: "r2-shared-swift"),
+                "Zip",
+            ],
             path: "./r2-streamer-swift/",
             exclude: ["Info.plist"],
             resources: [
@@ -34,7 +38,7 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "r2-streamer-swiftTests",
+            name: "R2StreamerTests",
             dependencies: ["R2Streamer"],
             path: "./r2-streamer-swiftTests/",
             exclude: ["Info.plist"],
